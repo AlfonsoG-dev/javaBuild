9d43d9b175ed00dc9f2a9f5be07cdf0a4b20262e
tuNovio <alfgomachi@gmail.com>
2023-11-08 03:26:51 -0500

    feat: clase de ayuda para crear las operaciones

--- src/Utils/OperationUtils.java
+++ src/Utils/OperationUtils.java
@@ -1,17 +1,30 @@
 package Utils;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
 import Operations.FileOperation;
 
 public class OperationUtils {
-    private FileUtils fileUtils;
     private FileOperation fileOperation;
     private String localPath;
     public OperationUtils(String nLocalPath) {
-        fileUtils = new FileUtils();
         fileOperation = new FileOperation(nLocalPath);
         localPath = nLocalPath;
+    }
+    public void CMDOutput(InputStream miInputStream) {
+        try {
+            BufferedReader mio = new BufferedReader(new InputStreamReader(miInputStream));
+            while(mio.read() != -1) {
+                System.out.println(mio.readLine());
+            }
+            mio.close();
+        } catch(Exception e) {
+            System.err.println(e);
+        }
     }
     public String srcClases() {
         String names = "";
@@ -19,20 +32,19 @@ public class OperationUtils {
             String[] srcdirs = fileOperation.listSRCDirectories("src").split("\n");
             for(String s: srcdirs) {
                 if(s.isEmpty() == false) {
-                    names += s + "* ";
+                    names += "." + s + "*.java ";
                 }
             }
         } catch(Exception e) {
             System.err.println(e);
         }
-        String cNames = "\\src\\* " + names.substring(0, names.length()-1);
-        System.out.println(cNames);
+        String cNames = ".\\src\\*.java " + names.substring(0, names.length()-1);
         return cNames;
     }
 
     public String libJars() {
         String names = "";
-        String[] libfiles = {""};
+        String[] libfiles = fileOperation.listLibFiles().split("\n");
         for(String l: libfiles) {
             if(l.contains(".jar")) {
                 names += l + "\n";
@@ -50,7 +62,7 @@ public class OperationUtils {
             }
         }
         String compileCommand = "";
-        if(b.isEmpty() == false) {
+        if(b.isEmpty() == true) {
              compileCommand = "javac -d .\\bin\\ " + srcClases;
         } else {
             String cb = b.substring(0, b.length()-1);
@@ -58,5 +70,37 @@ public class OperationUtils {
             compileCommand = "javac -d .\\bin\\ -cp " + forCommand;
         }
         return compileCommand;
+    }
+    public void CreateExtractionFiles(String[] jars) {
+        File extraction = new File(localPath + "\\extractionFiles");
+        if(extraction.exists() == false) {
+            extraction.mkdir();
+        }
+        String[] libNames = jars;
+        for(String n: libNames) {
+            fileOperation.CopyFilesfromSourceToTarget(n, extraction.getPath());
+        }
+    }
+    public String CreateExtractionCommand() throws IOException {
+        String command = "";
+        File extractionFile = new File(localPath + "\\extractionFiles");
+        for(File f: extractionFile.listFiles()) {
+            if(f.isDirectory()) {
+                for(File mf: f.listFiles()) {
+                    command += "cd " + mf.getParent() + " && " + "jar -xf " + mf.getName() + " && " + "rm -r " + mf.getName() + "\n";
+                }
+            }
+        }
+        return command;
+    }
+    public String CreateJarFileCommand() {
+        String command = "";
+        File extractionFile = new File(localPath + "\\extractionFiles");
+        String directory = "";
+        for(File f: extractionFile.listFiles()) {
+             directory += " -C " +f.getPath() + "\\ .";
+        }
+        command = "jar -cfm test.jar Manifesto.txt -C .\\bin\\ ." + directory;
+        return command;
     }
 }
