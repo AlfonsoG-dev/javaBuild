7fac1c0431689205d91b9758e6b242562b95ddd0
tuNovio <alfgomachi@gmail.com>
2023-11-08 03:24:34 -0500

    feat: adicionar opciones CLI

--- src/JavaBuild.java
+++ src/JavaBuild.java
@@ -1,17 +1,24 @@
 import Operations.Operation;
 class JavaBuild {
-    public static void main(String[] args){
+    public static void main(String[] args) {
+        //Operation mio = new Operation(".\\");
+        //mio.CreateJarOperation();
         for(int i=0; i<args.length; ++i) {
             Operation mia = new Operation(".\\");
             switch(args[i]) {
                 case "-c":
                     mia.CompileProyectOperation();
                     break;
+                case "-ex":
+                    mia.ExtractJarDependencies();
+                    break;
                 case "-cj":
                     mia.CreateJarOperation();
                     break;
                 case "--h":
-                    System.out.println("not implemented yet");
+                    System.out.println("use -c to compile the proyect");
+                    System.out.println("use -ex to extract the lib jar files");
+                    System.out.println("use -cj to create the proyect jar file");
                     break;
                 default: 
                     System.out.println("use --h for help");
