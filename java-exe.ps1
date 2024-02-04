98e754ae17298506596f778e0601811f8ca42bd2
AlfonsoG-dev <alfgomachi@gmail.com>
2024-02-04 13:13:41 -0500

    refactor: using the parent folder name

--- java-exe.ps1
+++ java-exe.ps1
@@ -1,5 +1,5 @@
 $compile = "javac -Xlint:all -d .\bin\ .\src\*.java .\src\Operations\*.java .\src\Utils\*.java "
-$createJar = "jar -cfm JavaBuild.jar Manifesto.txt -C .\bin\ ."
-$javaCommand = "java -jar JavaBuild.jar"
+$createJar = "jar -cfm javaBuild.jar Manifesto.txt -C .\bin\ ."
+$javaCommand = "java -jar javaBuild.jar"
 $runCommand = "$compile" + " && " + "$createJar" + " && " +"$javaCommand"
 Invoke-Expression $runCommand
