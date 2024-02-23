d6b6c257dc097256b07d3ef66bebdacb90b834e2
AlfonsoG-dev <alfgomachi@gmail.com>
2024-02-23 18:59:22 -0500

    refactor: use aggregate operations in lists methods

--- src/Operations/FileOperation.java
+++ src/Operations/FileOperation.java
@@ -3,10 +3,13 @@ package Operations;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+
 import java.nio.file.DirectoryStream;
 import java.nio.file.Files;
 import java.nio.file.StandardCopyOption;
 import java.nio.file.Path;
+
+import java.util.ArrayList;
 
 import Utils.FileUtils;
 import Utils.OperationUtils;
@@ -14,17 +17,22 @@ public class FileOperation {
 
     private FileUtils fileUtils;
     private String localPath;
+    private OperationUtils operationUtils;
     public FileOperation(String nLocalPath) {
-        fileUtils = new FileUtils();
         localPath = nLocalPath;
+        fileUtils = new FileUtils();
+        operationUtils = new OperationUtils(localPath);
     } 
     public void createFiles(String fileName, String mainClass, boolean includeExtraction) {
         try {
-            File localFile = new File(localPath);
-            OperationUtils utils = new OperationUtils(localFile.getPath());
-            File miFile = new File(localPath + "\\src");
+            File
+                localFile = new File(localPath),
+                miFile    = new File(localPath + "\\src");
+            FileWriter 
+                miFileWriter = null,
+                writeMainClass = null;
             if(fileName.equals(".gitignore")) {
-                FileWriter miFileWriter = new FileWriter(localFile.getPath() + "\\" + fileName);
+                miFileWriter = new FileWriter(localFile.getPath() + "\\" + fileName);
                 miFileWriter.write(
                         "**bin" + "\n" +
                         "**lib" + "\n" +
@@ -36,10 +44,10 @@ public class FileOperation {
                 );
                 miFileWriter.close();
             } else if(fileName.equals("Manifesto.txt")) {
-                String libJars = utils.libJars();
+                String libJars = operationUtils.libJars();
                 fileUtils.writeManifesto(localFile, "Manifesto.txt", includeExtraction, libJars);
             } else if(fileName.equals(mainClass + ".java")) {
-                FileWriter writeMainClass = new FileWriter(miFile.getPath() + "\\" + fileName);
+                writeMainClass = new FileWriter(miFile.getPath() + "\\" + fileName);
                 writeMainClass.write(
                         "class " + mainClass + " {\n" +
                         "    public static void main(String[] args) {\n" + 
@@ -55,48 +63,54 @@ public class FileOperation {
             e.printStackTrace();
         }
     }
-    public String listLibFiles() {
-        String names = "";
+    public ArrayList<String> listLibFiles() {
+        ArrayList<String> names = new ArrayList<>();
         try {
-            File libFile = new File(localPath + "\\lib");
-            if(libFile.exists() && libFile.listFiles() != null) {
-                DirectoryStream<Path> libFiles = Files.newDirectoryStream(libFile.toPath());
-                for(Path p: libFiles) {
-                    File f = p.toFile();
+            File lf = new File(localPath + "\\lib");
+            if(lf.listFiles() != null) {
+                DirectoryStream<Path> listFiles = Files.newDirectoryStream(lf.toPath());
+                listFiles.forEach(e -> {
+                    File f = e.toFile();
                     for(File mf: f.listFiles()) {
-                        names += mf.getPath() + "\n";
+                        names.add(mf.getPath());
                     }
-                }
+                });
             }
         } catch(Exception e) {
             e.printStackTrace();
         }
         return names;
     }
-    public String listSRCDirectories(String path) throws IOException {
-        String cPath = fileUtils.getCleanPath(path);
-        File localFile = new File(localPath + "\\" + cPath);
-        String names = "";
-        if(localFile.listFiles() != null) {
-            DirectoryStream<Path> localFiles = Files.newDirectoryStream(localFile.toPath());
-            for(Path p: localFiles) {
-                File f = p.toFile();
+    public ArrayList<String> listSRCDirectories(String path) throws IOException {
+        ArrayList<String> names = new ArrayList<>();
+        String c = fileUtils.getCleanPath(path);
+        File lf = new File(localPath + "\\" + c);
+        if(lf.listFiles() != null) {
+            DirectoryStream<Path> listFiles = Files.newDirectoryStream(lf.toPath());
+            listFiles.forEach(e -> {
+                File f = e.toFile();
                 if(f.isDirectory()) {
-                    names += f.getPath() + "\\" + "\n";
+                    names.add(f.getPath() + "\\");
                     if(f.listFiles() != null) {
-                        names += listSRCDirectories(f.getPath());
+                        try {
+                            names.addAll(
+                                    listSRCDirectories(f.getPath())
+                            );
+                        } catch(Exception err) {
+                            err.printStackTrace();
+                        }
                     }
                 }
-            }
+            });
         }
         return names;
     }
     public boolean extractionDirContainsPath(String libJars) throws IOException {
         boolean containsPath = false;
         File extractionFile = new File(localPath + "\\extractionFiles");
-        File miFile = new File(libJars);
-        if(miFile.getParent() != null && extractionFile.listFiles() != null) {
-            String jarLibParent = miFile.getParent();
+        File myFile = new File(libJars);
+        if(myFile.getParent() != null && extractionFile.listFiles() != null) {
+            String jarLibParent = myFile.getParent();
             String jarNameParent = new File(jarLibParent).getName();
             for(File f: extractionFile.listFiles()) {
                 String extractionDirName = new File(f.getCanonicalPath()).getName();
@@ -109,28 +123,29 @@ public class FileOperation {
     }
     public void copyFilesfromSourceToTarget(String sourceFilePath, String targetFilePath) {
         try {
-            if(new File(sourceFilePath).isFile()) {
-                String sourceFileName = new File(sourceFilePath).getName();
-                String sourceParent = new File(sourceFilePath).getParent();
+            File sf = new File(sourceFilePath);
+            if(sf.isFile()) {
+                String sourceFileName = sf.getName();
+                String sourceParent = sf.getParent();
                 String sourceParentName = new File(sourceParent).getName();
-                File targetFile = new File(
+                File tf = new File(
                         targetFilePath + "\\" +
                         sourceParentName + "\\" +
                         sourceFileName
                 );
                 fileUtils.createParentFile(
-                        targetFile.getPath(),
-                        targetFile.getParent()
+                        tf.getPath(),
+                        tf.getParent()
                 );
                 System.out.println(
                         Files.copy(
-                            new File(sourceFilePath).toPath(),
-                            targetFile.toPath(),
+                            sf.toPath(),
+                            tf.toPath(),
                             StandardCopyOption.COPY_ATTRIBUTES
                         )
                 );
-            } else if(new File(sourceFilePath).isDirectory()) {
-                String[] fileNames = fileUtils.listFilesFromPath(sourceFilePath).split("\n");
+            } else if(sf.isDirectory()) {
+                ArrayList<String> fileNames = fileUtils.listFilesFromPath(sourceFilePath);
                 for(String fn: fileNames) {
                     File sourceFile = new File(fn);
                     String cTargetNames = fileUtils.createTargetFromParentPath(
