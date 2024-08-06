7c288c8b8470fd6acce371aefaf7683986d77046
AlfonsoG-dev <alfgomachi@gmail.com>
2024-08-05 22:43:05 -0500

    refactor: windows build script file

---
+++ java-exe.ps1
@@ -0,0 +1,7 @@
+$srcClases = ".\src\*.java .\src\Utils\*.java .\src\Operations\*.java "
+$libFiles = ""
+$compile = "javac -Werror -Xlint:all -d .\bin\ $srcClases"
+$createJar = "jar -cfm JavaBuild.jar Manifesto.txt -C .\bin\ ."
+$javaCommand = "java -jar JavaBuild.jar"
+$runCommand = "$compile" + " && " + "$createJar" + " && " +"$javaCommand"
+Invoke-Expression $runCommand 
