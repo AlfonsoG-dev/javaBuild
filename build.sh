9b561702d2ecb9293cdb056eaf58ae93b3567406
AlfonsoG-dev <alfgomachi@gmail.com>
2024-08-05 22:27:48 -0500

    build file for linux

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
-javac -Werror -Xlint:all -d .\bin\ .\src\*.java .\src\Operations\*.java .\src\Utils\*.java
-jar -cfm JavaBuild.jar Manifesto.txt -C .\bin\ .
+javac -Werror -Xlint:all -d ./bin/ ./src/*.java ./src/Operations/*.java ./src/Utils/*.java
+jar -cfm JavaBuild.jar Manifesto.txt -C ./bin/ .
 java -jar JavaBuild.jar
