69747e5aa63b88d28c96af15e165e7d30e46d3c0
AlfonsoG-dev <alfgomachi@gmail.com>
2025-06-18 16:45:17 -0500

    fix: exception control

--- src/application/operations/ExecutorOperation.java
+++ src/application/operations/ExecutorOperation.java
@@ -1,12 +1,12 @@
 package operations;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.concurrent.Callable;
+import java.util.concurrent.CancellationException;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.FutureTask;
+import java.util.concurrent.RejectedExecutionException;
 
 public class ExecutorOperation {
     
@@ -16,12 +16,17 @@ public class ExecutorOperation {
         System.out.println("[Info] Starting computation");
         try(ExecutorService executor = Executors.newCachedThreadPool()) {
             executor.submit(future);
-            // TODO: verify capture exception of future when invoke `get()`
-            t = future.get();
-            System.out.println("[Info] Waiting to get the results...");
+            try {
+                t = future.get();
+                System.out.println("[Info] Waiting to get the results...");
+            } catch(CancellationException cancelException) {
+                System.err.println("[Error] Future was cancelled " + cancelException.getCause());
+            }
+        } catch(NullPointerException | RejectedExecutionException taskException) {
+            System.err.println("[Error] Task execution not allowed " + taskException.getCause());
         } catch (InterruptedException | ExecutionException e) {
             Thread.currentThread().interrupt();
-            System.err.println("[Error] Execution was interrupted");
+            System.err.println("[Error] Execution was interrupted " + e.getCause());
         }
         return t;
     }
