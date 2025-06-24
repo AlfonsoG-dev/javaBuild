006e1b31e517c581961ca1f2fee852b4526d4467
AlfonsoG-dev <alfgomachi@gmail.com>
2025-06-23 21:31:43 -0500

    refactor: fix grammar errors

--- src/application/operations/ExecutorOperation.java
+++ src/application/operations/ExecutorOperation.java
@@ -14,20 +14,19 @@ public class ExecutorOperation {
      * Helper to execute a callable task with virtual threads.
      * @param task the Callable task to execute 
      * @throws InterruptedException if the executor service can't start
-     * @throws CancellationException if the future of the task was cancelled.
+     * @throws CancellationException if the future of the task was canceled.
      * @return the result of the execution of the generic type T
      */
     public <T> T executeConcurrentCallableList(Callable<T> task) {
         T result = null;
         ExecutorService executor = Executors.newCachedThreadPool();
         System.out.println("[Info] Starting computation");
-    
         try {
             Future<T> future = executor.submit(task);
             System.out.println("[Info] Waiting to get the results...");
             result = future.get();
         } catch (CancellationException cancelException) {
-            System.err.println("[Error] Future was cancelled: " + cancelException.getMessage());
+            System.err.println("[Error] Future was canceled: " + cancelException.getMessage());
         } catch (RejectedExecutionException executionException) {
             System.err.println("[Error] Task execution rejected: " + executionException.getMessage());
         } catch (InterruptedException interruptedException) {
